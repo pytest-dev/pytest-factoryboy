cc944c8f188e2406dfede31e2724a182292fba65
Joshua Crim <jshcrm@gmail.com>
2017-08-06 15:21:13 +0200

    Update compat.py

--- pytest_factoryboy/compat.py
+++ pytest_factoryboy/compat.py
@@ -2,7 +2,7 @@
 
 
 def get_fixture_value(request, *args):
-    """Compatibility check for pytest2 FixtureRequest.getfunctargvalue method."""
+    """Compatibility check for pytest2 FixtureRequest.getfuncargvalue method."""
     try:
         return request.getfixturevalue(*args)
     except AttributeError:
