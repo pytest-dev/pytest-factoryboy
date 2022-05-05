132838005baede239d7886d612cbf82aa0692d1f
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2022-05-05 14:58:12 +0200

    fix: add missing type hintings for hooks

--- pytest_factoryboy/hooks.py
+++ pytest_factoryboy/hooks.py
@@ -1,5 +1,7 @@
 """pytest-factoryboy pytest hooks."""
 
+from pytest import FixtureRequest
 
-def pytest_factoryboy_done(request):
+
+def pytest_factoryboy_done(request: FixtureRequest) -> None:
     """Called after all factory based fixtures and their post-generation actions were evaluated."""
